8a5633859286af8d5324ee3d1d209fa55e3cb3a1
Sergei Bogolepov <s.bogolepov@wis.software>
2018-02-22 22:35:46 +0300

    Added real-time updates after actions on containers. Added actions (create, start, stop, delete) over containers. The code of subclass of WebSocketHandler has been redesigned.

--- hello_world.py
+++ hello_world.py
@@ -48,28 +48,47 @@ class DockerWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
         logging.info("WebSocket opened")
 
+    def _url_address(self, **kwargs):
+        if kwargs["tag_image"] == "default":
+            kwargs["tag_image"] = "default{}".format(random.randint(1, 100000))
+        url = "{}.git".format(kwargs["url_address"])
+        for line in CLIENT.build(path=url, rm=True, tag=kwargs["tag_image"]):
+            self.write_message(dict(output=line.decode("utf-8")))
+
+    def _images(self, **kwargs):
+        self.write_message(dict(
+            images=[i["RepoTags"][0] for i in CLIENT.images()]))
+
+    def _containers(self, **kwargs):
+        self.write_message(dict(
+            containers=list(filter(
+                lambda x: x[0] != os.getenv("PROJ_CONT"),
+                [(i["Names"][0], i["Status"]) for i in CLIENT.containers(
+                    all=True)]))))
+
+    def _create(self, **kwargs):
+        CLIENT.create_container(image=kwargs["elem"], command='/bin/sleep 999')
+        self._containers()
+
+    def _start(self, **kwargs):
+        CLIENT.start(container=kwargs["elem"])
+        self._containers()
+
+    def _stop(self, **kwargs):
+        CLIENT.stop(container=kwargs["elem"], timeout=1)
+        self._containers()
+
+    def _remove(self, **kwargs):
+        CLIENT.remove_container(container=kwargs["elem"])
+        self._containers()
+
     def on_message(self, message):
         d = json.loads(message)
-        if "url-address" in d:
-            if d["tag_image"] == "default":
-                d["tag_image"] = "default{}".format(random.randint(1, 100000))
-            url = "{}.git".format(d["url-address"])
-            for line in CLIENT.build(path=url, rm=True, tag=d["tag_image"]):
-                self.write_message(dict(output=line.decode("utf-8")))
-        elif "images" in d:
-            self.write_message(dict(
-                images=[i["RepoTags"][0] for i in CLIENT.images()]))
-        elif "containers" in d:
-            self.write_message(dict(
-                containers=list(filter(
-                    lambda x: x != os.getenv("PROJ_CONT"),
-                    [i["Names"][0] for i in CLIENT.containers(all=True)]))))
-        elif "run" in d:
-            container = CLIENT.create_container(
-                image=d["elem"], command='/bin/sleep 9000')
-            CLIENT.start(container=container.get("Id"))
-        elif "stop" in d:
-            CLIENT.stop(container=d["elem"])
+        general = dict(url_address=self._url_address, images=self._images,
+                       containers=self._containers, create=self._create,
+                       start=self._start, stop=self._stop, remove=self._remove)
+        if d["method"] in general.keys():
+            general[d["method"]](**d)
         else:
             self.write_message(dict(message="Client error"))
 
--- static/websockets.js
+++ static/websockets.js
@@ -2,18 +2,25 @@ function load(key_from_html, elem) {
         var ws = new WebSocket("ws://" + location.hostname + ":8888" + "/load_from_docker/");
         ws.onopen = function() {
            var dict = {};
-           if (key_from_html == "url-address") {
+           if (key_from_html == "url_address") {
            $("#print_output").empty();
+           dict["method"] = key_from_html;
            dict[key_from_html] = $("#url").val();
            dict["tag_image"] = $("#tag_image").val() || "default";
-           } else if (key_from_html == "run") {
-           dict[key_from_html] = key_from_html;
+           } else if (key_from_html == "create") {
+           dict["method"] = key_from_html;
            dict["elem"] = elem;
+           } else if (key_from_html == "start") {
+           dict["method"] = key_from_html;
+           dict["elem"] = elem.slice(1);
            } else if (key_from_html == "stop") {
-           dict[key_from_html] = key_from_html;
+           dict["method"] = key_from_html;
+           dict["elem"] = elem.slice(1);
+           } else if (key_from_html == "remove") {
+           dict["method"] = key_from_html;
            dict["elem"] = elem.slice(1);
            } else {
-           dict[key_from_html] = key_from_html;
+           dict["method"] = key_from_html;
            };
            dict = JSON.stringify(dict);
            ws.send(dict);
@@ -22,23 +29,36 @@ function load(key_from_html, elem) {
            var getjson = evt.data;
            var parsejson = JSON.parse(getjson);
            if (key_from_html == "images") {$("#print_images").empty();};
-           if (key_from_html == "containers") {$("#print_containers").empty();};
+           if (key_from_html == "containers" || key_from_html == "create" ||
+           key_from_html == "start" || key_from_html == "stop" ||
+           key_from_html == "remove") {$("#print_containers").empty();};
            switch (key_from_html) {
               case "images":
                  for (key in parsejson.images) {
                  $("#print_images").append("<li>" + parsejson.images[key] +
-                 "</li>" + "<button onclick='load(\"run\", \"" + parsejson.images[key] +
-                 "\")'>Run Container</button>");
+                 "</li>" + "<button onclick='load(\"create\", \"" + parsejson.images[key] +
+                 "\")'>Create Container</button>");
                  };
                  break;
+              case "create":
+              case "start":
+              case "stop":
+              case "remove":
               case "containers":
                  for (key in parsejson.containers) {
-                 $("#print_containers").append("<li>" + parsejson.containers[key] +
-                 "</li>" + "<button onclick='load(\"stop\", \"" + parsejson.containers[key] +
-                 "\")'>Stop Container</button>");
+                 if (parsejson.containers[key][1].indexOf("Up") == 0) {
+                 $("#print_containers").append("<li>" + parsejson.containers[key][0] +
+                 "<br/>" + parsejson.containers[key][1] + "</li>" +
+                 "<button onclick='load(\"stop\", \"" + parsejson.containers[key][0] +
+                 "\")'>Stop Container</button>");} else {
+                 $("#print_containers").append("<li>" + parsejson.containers[key][0] +
+                 "<br/>" + parsejson.containers[key][1] + "</li>" +
+                 "<button onclick='load(\"start\", \"" + parsejson.containers[key][0] +
+                 "\")'>Start Container</button>" + "<button onclick='load(\"remove\", \"" +
+                 parsejson.containers[key][0] + "\")'>Remove Container</button>");}
                  };
                  break;
-              case "url-address":
+              case "url_address":
                  for (key in parsejson) {
                  $("#print_output").append("<li>" + parsejson[key] + "</li>");
                  };
