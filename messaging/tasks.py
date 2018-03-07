e3f2f544dab5419619a66ec3473393cca29da048
Sergei Bogolepov <s.bogolepov@wis.software>
2018-03-07 18:24:23 +0300

    Edited salary, tornado output to client(2)

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -1,47 +1,29 @@
 """Tasks description module."""
 import random
+import json
+
 import docker
 
 from celery import Celery
 
-APP = Celery('tasks', backend='rpc://', broker='amqp://', include=['messaging.tasks'])
+APP = Celery('tasks', backend='rpc://', broker='amqp://',
+             include=['messaging.tasks'])
 
-# CLIENT = docker.APIClient(base_url='unix://var/run/docker.sock')
+CLIENT = docker.APIClient(base_url='unix://var/run/docker.sock')
+
 
 @APP.task
 def build_image(**kwargs):
     """Builds docker image with specified parameters."""
-    client = docker.APIClient(base_url='unix://var/run/docker.sock')
 
     if kwargs["tag_image"] == "default":
         kwargs["tag_image"] = "default{}".format(random.randint(1, 100000))
 
     url = "{}.git".format(kwargs["url_address"])
 
-    # output = [line for line in CLIENT.build(path=url, rm=True, tag=kwargs["tag_image"])]
-
     lines = []
 
-    for line in client.build(path=url, rm=True, tag=kwargs["tag_image"]):
-        lines.append(line)
-        build_image.update_state(state='PROGRESS', meta={'line': lines})
-    # return 0
-
-
-@APP.task(ignore_result=True)
-def print_hello():
-    """Test task."""
-    print('hello there')
-
-
-@APP.task
-def gen_prime(value):
-    """Long lasting test task."""
-    multiples = []
-    results = []
-    for i in range(2, value+1):
-        if i not in multiples:
-            results.append(i)
-            for j in range(i*i, value+1, i):
-                multiples.append(j)
-    return results
+    for line in CLIENT.build(path=url, rm=True, tag=kwargs["tag_image"]):
+        lines.append(list(json.loads(line).values())[0])
+        build_image.update_state(state='PROGRESS', meta={
+            'line': lines, 'method': kwargs['method']})
