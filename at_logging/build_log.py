0102d8b0b482c5a86d6b32767448aa76d2d2a25b
ukhryab <mihail.sibirev@mail.ru>
2018-03-09 18:48:25 +0300

    add directory exists check

--- at_logging/build_log.py
+++ at_logging/build_log.py
@@ -1,12 +1,18 @@
 import logging
 from os import getcwd
+from pathlib import Path
+
+_LOGNAME = "build_log"
+_LOGDIR = getcwd() + "/logs/"
 
 _FORMAT_STR = u'%(levelname)-8s [%(asctime)s] %(message)s'
 
-_LOG = logging.getLogger("build_log")
+_LOG = logging.getLogger(_LOGNAME)
 _LOG.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler(getcwd() + "/logs/build.log")
+Path(_LOGDIR).mkdir(parents=True, exist_ok=True)
+
+file_handler = logging.FileHandler(_LOGDIR + _LOGNAME + ".log")
 file_handler.setFormatter(logging.Formatter(_FORMAT_STR))
 _LOG.addHandler(file_handler)
 
