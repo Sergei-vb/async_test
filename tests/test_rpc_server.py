15cf9aed3a921636cebb6c06ca32ac0c43f97ef9
ukhryab <mihail.sibirev@mail.ru>
2018-04-23 16:08:07 +0300

    Add images_init, containers_init in setUp. Fix 'test_create' method. Do some refactoring.

--- tests/test_rpc_server.py
+++ tests/test_rpc_server.py
@@ -3,7 +3,7 @@ from tornado.testing import AsyncHTTPTestCase
 from tornado.testing import gen_test
 from tornado.escape import json_decode, json_encode
 
-from tests.sub_docker_websocket import SubDockerWebSocket
+from tests.sub_docker_websocket import SubDockerWebSocket, CLIENT
 
 
 def make_app():
@@ -15,18 +15,6 @@ def make_app():
 
 class TestWebSocket(AsyncHTTPTestCase):
     """RPC procedures test."""
-    def setUp(self):
-        super().setUp()
-        self.container_name = "87541"
-        self.user_id = "5"
-        self.path = "/load_from_docker"
-        self.request = self.path + "/?user_id={}".format(self.user_id)
-        self.url_address = "https://github.com/Sergei-vb/coralline-rpc"
-        self.tag_image = "{}/tag_from_test:latest".format(self.user_id)
-
-    def get_app(self):
-        app = make_app()
-        return app
 
 #
 # Private methods
@@ -48,11 +36,68 @@ class TestWebSocket(AsyncHTTPTestCase):
 
         return json_decode(response)
 
+    def images_init(self):
+        CLIENT.images_list.append(
+            {"tag": self.tag_image, "available": True}
+        )
+
+    def containers_init(self):
+        containers_list = [{'Image': "alpine:3.7",
+                            'Command': "/bin/sleep 999",
+                            'Labels': {'out': ''},
+                            'State': 'created',
+                            'Created': 1524205394,
+                            'Status': 'Created',
+                            'Names': ["/" + self.container_to_run],
+                            },
+                           {'Image': "alpine:3.7",
+                            'Command': "/bin/sleep 999",
+                            'Labels': {'out': ''},
+                            'State': 'running',
+                            'Created': 1524205394,
+                            'Status': 'Up 15 minutes',
+                            'Names': ["/" + self.container_running],
+                            },
+                           {'Image': "alpine:3.7",
+                            'Command': "/bin/sleep 999",
+                            'Labels': {'out': ''},
+                            'State': 'created',
+                            'Created': 1524205394,
+                            'Status': 'Created',
+                            'Names': ["/" + self.container_to_remove],
+                            },
+                           ]
+        CLIENT.containers_list.extend(containers_list)
+
 #
 # Tests
 #
 
     # General tests. #
+
+    def setUp(self):
+        super().setUp()
+        self.user_id = "5"
+        self.path = "/load_from_docker"
+        self.request = self.path + "/?user_id={}".format(self.user_id)
+
+        self.url_address = "https://github.com/Sergei-vb/coralline-rpc"
+        self.new_tag_image = "{}/new_tag:latest".format(self.user_id)
+        self.tag_image = "{}/test_tag:latest".format(self.user_id)
+
+        self.container_new = "test_create"
+        self.container_to_run = "test_start"
+        self.container_running = "test_running"
+        self.container_to_remove = "test_remove"
+
+        self.images_init()
+        self.containers_init()
+
+    def get_app(self):
+        app = make_app()
+        return app
+
+    # HTTP tests
 
     def test_root(self):
         response = self.fetch('/')
@@ -69,14 +114,22 @@ class TestWebSocket(AsyncHTTPTestCase):
 
     # RPC procedures tests. #
 
-    # --- needs improvement --- #
+    # --- needs improvement: add websocket connection message --- #
     @gen_test
     def test_websocket_connection(self):
         message = {"method": None, "elem": None}
         response = yield self._get_response(message)
 
         self.assertEqual('Client error', response["message"])
-        # TODO: websocket connection message
+
+    # TODO
+    @gen_test
+    def test_url_address(self):
+        # message = {"method": "url_address",
+        #            "url_address": self.url_address,
+        #            "tag_image": self.tag_image}
+        # response = yield self._get_response(message)
+        pass
 
     # TODO
     @gen_test
@@ -92,15 +145,7 @@ class TestWebSocket(AsyncHTTPTestCase):
 
         self.assertIn(self.tag_image, images)
 
-    # TODO
-    @gen_test
-    def test_url_address(self):
-        message = {"method": "url_address",
-                   "url_address": self.url_address,
-                   "tag_image": self.tag_image}
-        response = yield self._get_response(message)
-
-    # TODO
+    # OK
     @gen_test
     def test_containers(self):
         message = {"method": "containers"}
@@ -111,36 +156,38 @@ class TestWebSocket(AsyncHTTPTestCase):
         self.assertIsInstance(response["containers"], list)
         self.assertNotEqual(len(response["containers"]), 0)
 
-        container_name = "/" + self.container_name
+        container_name = "/" + self.container_to_run
 
         containers = {i[0]: i[1] for i in response["containers"]}
         self.assertIn(container_name, containers.keys(),
                       "Container not found")
 
-    # TODO
+    # OK
     @gen_test
     def test_create(self):
+        cont_num = len(CLIENT.containers_list)
+
         message = {"method": "create",
-                   "elem": "5/tag_from_test:latest"}
+                   "elem": self.tag_image}
         response = yield self._get_response(message)
 
         self.assertIsInstance(response, dict)
         self.assertEqual(response["method"], "create")
         self.assertIsInstance(response["containers"], list)
-        # TODO: check existed tag
+        self.assertEqual(len(response["containers"]), cont_num+1)
 
     # OK
     @gen_test
     def test_start(self):
         message = {"method": "start",
-                   "elem": self.container_name}
+                   "elem": self.container_to_run}
         response = yield self._get_response(message)
 
         self.assertIsInstance(response, dict)
         self.assertEqual(response["method"], "start")
         self.assertIsInstance(response["containers"], list)
 
-        container_name = "/" + self.container_name
+        container_name = "/" + self.container_to_run
 
         containers = {i[0]: i[1] for i in response["containers"]}
         self.assertIn(container_name, containers.keys(),
@@ -153,19 +200,20 @@ class TestWebSocket(AsyncHTTPTestCase):
     # OK
     @gen_test
     def test_stop(self):
-        message = {"method": "start",
-                   "elem": self.container_name}
-        response = yield self._get_response(message)
+        # First, start a container:
+        # message = {"method": "start",
+        #            "elem": self.container_running}
+        # response = yield self._get_response(message)
 
         message = {"method": "stop",
-                   "elem": self.container_name}
+                   "elem": self.container_running}
         response = yield self._get_response(message)
 
         self.assertIsInstance(response, dict)
         self.assertEqual(response["method"], "stop")
         self.assertIsInstance(response["containers"], list)
 
-        container_name = "/" + self.container_name
+        container_name = "/" + self.container_running
 
         containers = {i[0]: i[1] for i in response["containers"]}
         self.assertIn(container_name, containers.keys(),
@@ -179,13 +227,13 @@ class TestWebSocket(AsyncHTTPTestCase):
     @gen_test
     def test_remove(self):
         message = {"method": "remove",
-                   "elem": self.container_name}
+                   "elem": self.container_to_remove}
         response = yield self._get_response(message)
         self.assertIsInstance(response, dict)
         self.assertEqual(response["method"], "remove")
         self.assertIsInstance(response["containers"], list)
 
-        container_name = "/" + self.container_name
+        container_name = "/" + self.container_to_remove
 
         containers = {i[0]: i[1] for i in response["containers"]}
         self.assertNotIn(container_name, containers.keys(),
