a0ee9e34ec03bc813efa3a50ba2f162fe5387ab3
Sergei-vb <s.bogolepov@wis.software>
2018-04-24 17:16:48 +0300

    Edit mock_django_orm, images_list in mock_docker.

--- tests/mock_django_orm.py
+++ tests/mock_django_orm.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
+"""Mock Django ORM."""
 
 
 class QuerySet:
+    """Imitates work with queryset after filter."""
     def __init__(self, data=None):
-        self.collect = []
-        self.data = data
+        self.collect = data if data else []
 
     def __iter__(self):
         return iter(self.collect)
 
-    def __repr__(self):
-        if self.data is None:
-            return "<QuerySet [<UserImage: UserImage>]>"
-        else:
-            return "<QuerySet {0}>".format(self.data)
-
     def append(self, obj):
+        """Adds object to queryset."""
         self.collect.append(obj)
 
     def values(self):
+        """Ensures that code works correctly."""
         return QuerySet(self.collect)
 
     def exclude(self, tag=None):
-        self.collect = [i for i in self.data if i["tag"] != tag]
+        """Excludes objects from queryset."""
+        self.collect = [i for i in self.collect if i["tag"] != tag]
         return QuerySet(self.collect)
 
 
+# pylint: disable=too-few-public-methods
 class Database:
+    """Stores data."""
     collect = []
 
     def __init__(self, data):
@@ -34,35 +34,40 @@ class Database:
 
 
 class Objects:
+    """Provides access to objects."""
 
     @staticmethod
     def all():
+        """Gives objects all."""
         return Database.collect
 
     @staticmethod
     def filter(tag=None, user_id=None):
+        """Gives objects filter."""
         key = "tag" if tag else "user_id" if user_id else None
         val = tag if tag else user_id if user_id else None
+        answer = QuerySet()
         if key:
-            answer = QuerySet()
             for obj in Database.collect:
                 if obj[key] == val:
                     answer.append(obj)
-            if answer.collect:
-                return answer
+        return answer
 
 
+# pylint: disable=too-few-public-methods
 class UserImage:
+    """Creates Model."""
     objects = Objects()
 
-    def __init__(self, user_id, image_id, tag, created, size):
-        self.user_id = user_id
-        self.image_id = image_id
-        self.tag = tag
-        self.created = created
-        self.size = size
+    def __init__(self, **kwargs):
+        self.user_id = kwargs["user_id"]
+        self.image_id = kwargs["image_id"]
+        self.tag = kwargs["tag"]
+        self.created = kwargs["created"]
+        self.size = kwargs["size"]
 
     def save(self):
+        """Saves model."""
         inst = dict(user_id=self.user_id,
                     image_id=self.image_id,
                     tag=self.tag,
