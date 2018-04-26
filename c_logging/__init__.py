564cbbf9835dee297ea53dce15a726a4d5d5f9cc
ukhryab <mihail.sibirev@mail.ru>
2018-04-26 17:45:04 +0300

    Refactor logging.

---
+++ c_logging/__init__.py
@@ -0,0 +1,17 @@
+"""Logging of images building output."""
+import logging
+from os import getcwd
+from pathlib import Path
+
+_LOGNAME = "build_log"
+_LOGDIR = getcwd() + "/logs/"
+
+_FORMAT_STR = u'%(levelname)-8s [%(asctime)s] %(message)s'
+
+Path(_LOGDIR).mkdir(parents=True, exist_ok=True)
+
+_FILE_HANDLER = logging.FileHandler(_LOGDIR + _LOGNAME + ".log")
+_FILE_HANDLER.setFormatter(logging.Formatter(_FORMAT_STR))
+
+APP_LOG = logging.getLogger(_LOGNAME)
+APP_LOG.addHandler(_FILE_HANDLER)
