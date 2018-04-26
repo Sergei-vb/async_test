0156c120a298daec9bab2fe9e1065033a93fc775
ukhryab <mihail.sibirev@mail.ru>
2018-04-26 14:32:37 +0300

    Refactor 'tasks' module. Create 'save_to_database' function.

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -11,12 +11,33 @@ from messaging.app import APP
 CLIENT = docker.APIClient(base_url='unix://var/run/docker.sock')
 
 
+def save_to_database(user_id, tag_image):
+    """Saves user image to database. """
+
+    def tag_in_repotags(image):
+        return image["RepoTags"][0] == tag_image
+
+    images = CLIENT.images()
+    image_item = list(filter(tag_in_repotags, images))[0]
+
+    image_id = image_item["Id"]
+    created = datetime.datetime.fromtimestamp(image_item["Created"])
+    size = image_item["Size"]
+
+    user_image = UserImage(user_id=user_id,
+                           image_id=image_id,
+                           tag=tag_image,
+                           created=created,
+                           size=size
+                          )
+    user_image.save()
+
+
 @APP.task
 def build_image(user_id, **kwargs):
     """Builds docker image with specified parameters."""
 
     build_log.write('Started building an image...')
-
     build_log.write("USER_ID: {}".format(user_id))
 
     url = "{}.git".format(kwargs["url_address"])
@@ -29,26 +50,13 @@ def build_image(user_id, **kwargs):
 
     for line in CLIENT.build(path=url, rm=True, tag=tag_image):
         line_str = list(json.loads(line).values())[0]
-
-        build_log.write(line_str)
-
         lines.append(line_str)
+
         build_image.update_state(state='PROGRESS',
                                  meta={'line': lines,
                                        'method': kwargs['method']}
                                 )
 
-    image_id = list(filter(lambda x: x["RepoTags"][0] == tag_image,
-                           CLIENT.images()))[0]["Id"]
-    created = datetime.datetime.fromtimestamp(
-        list(filter(lambda x: x["RepoTags"][0] == tag_image,
-                    CLIENT.images()))[0]["Created"])
-    size = list(filter(lambda x: x["RepoTags"][0] == tag_image,
-                       CLIENT.images()))[0]["Size"]
-    user_image = UserImage(
-        user_id=user_id,
-        image_id=image_id,
-        tag=tag_image,
-        created=created,
-        size=size)
-    user_image.save()
+        build_log.write(line_str)
+
+    save_to_database(user_id, tag_image)
